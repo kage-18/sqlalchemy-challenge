20f62ded98d544d4877cbc31dafbbf4bfee09c4f
Karen Gutierrez <kareng018@berkeley.edu>
2021-09-10 10:42:27 -0700

    commiting before adding routes to app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Dependencies
 import numpy as np
 import datetime as dt
 
@@ -8,3 +9,15 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+#Setup
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#Setup Flask
+app = Flask(__name__)
+
+#Routes
